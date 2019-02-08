6d995dd232ab0d690d6894071c27e670e5062772
Eric Algera <ericalgera@gmail.com>
2019-02-08 09:22:46 +0100

    Commit voor wijzigingen aan Ubuntu VM

--- 10/hangman.py
+++ 10/hangman.py
@@ -1,28 +1,48 @@
 from string import ascii_lowercase
 import sys
+import os
 
 from movies import get_movie as get_word  # keep interface generic
 from graphics import hang_graphics
 
 ASCII = list(ascii_lowercase)
-HANG_GRAPHICS = list(hang_graphics())
-ALLOWED_GUESSES = len(HANG_GRAPHICS)
+HANG_GRAPHICS = list(hang_graphics()) # 07 Plaatjes
+ALLOWED_GUESSES = len(HANG_GRAPHICS)  # = 7
 PLACEHOLDER = '_'
 
 
 class Hangman(object):
-    pass 
+    def __init__(self, _w):
+        self.word = _w
+        self.letters_found = []
+        self.word_sofar = [PLACEHOLDER if i != " " else " " for i in _w]
+
+    def print_word_sofar(self):
+        for letter in self.word_sofar:
+            print(letter, end="")
+        print()
+        for i in range(len(self.word_sofar)):
+            print(i%10, end="")
+        print()
+        print()
+        print(self.word)
+
 
 # or use functions ...
-
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         word = sys.argv[1]
     else:
         word = get_word()
-    #print(word)
-    for i, graph in enumerate(HANG_GRAPHICS):
-        print(i, graph)
+    
+# init / call progra
+hm = Hangman(word)
+playing = True
+os.system('cls')
 
-    # init / call program
+while playing:
+
+    hm.print_word_sofar()
+
+# Game Over...
